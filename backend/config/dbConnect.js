29ad717e996319d85cf6b6a32a287a0950d721e5
hakynbayo <akinbayo.akinwande@decagon.dev>
2023-03-25 15:53:55 +0100

    backend-auth

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
     // connect DB
-mongoose.connect(process.env.MONGODB_KEY ,{ useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URL ,{ useUnifiedTopology: true,
 useNewUrlParser: true}).then(()=>{
     console.log('connected to DB')
 }).catch(err=>{
